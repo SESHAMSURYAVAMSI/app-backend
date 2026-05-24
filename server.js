343fac04fd50cba7d82c2065f8019e11f4702602
SESHAMSURYAVAMSI <suryavamsis005@gmail.com>
2026-05-24 13:17:37 +0530

    updated

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin:[ "http://localhost:3000",
+    "https://aig-app-two.vercel.app",],
     credentials: true,
   })
 );
